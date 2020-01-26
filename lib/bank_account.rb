f0ae584a412cbdb41f1c423fcc08ecfeed6e899f
Pledge Davis <pledgedavis14@gmail.com>
2020-01-26 01:52:38 +0000

    Done.

--- lib/bank_account.rb
+++ lib/bank_account.rb
@@ -1,3 +1,55 @@
+require 'pry'
 class BankAccount
+  attr_reader :name
+  attr_accessor :balance, :status 
+   @@account = []
+def initialize(name)
+@name = name 
+@balance = 1000
+@status = "open"
+@@account = []
+end
+
+def deposit(money)
+# binding.pry 
+# @@account << @balance
+# @@account << @balance*2
+# [2000].join("")
+@balance = @balance + money
+end
+ 
+ def display_balance
+   @balance
+ end
+
+
+
+
+
+
+
+
 
 end
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
